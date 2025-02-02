169593922e7f15e2f3dca425a0e1e9070dda7545
Florian Zunke <florianzunke@gmx.de>
2025-02-02 16:16:23 +0100

    added hover in 3 Sections

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { LandingpageComponent } from './landingpage/landingpage.component';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 import { MainContentComponent } from './main-content/main-content.component';
 
--- src/app/shared/components/reference-card/reference-card.component.ts
+++ src/app/shared/components/reference-card/reference-card.component.ts
@@ -21,9 +21,9 @@ export class ReferenceCardComponent {
       zitat: 'Florian hat während unserer Projektarbeit hervorragende Arbeit geleistet. Seine Fähigkeit, komplexe Logik in verlässlichen Code zu übertragen, hat unser Projekt maßgeblich vorangebracht. Des weiteren schätze ich besonders seine Zuverlässigkeit und die angenehme Zusammenarbeit mit ihm.'
     },
     {
-      name: 'A.Albrecht',
-      position: 'Team Partner',
-      zitat: 'Die Zusammenarbeit mit Florian war stets kommunikativ und lösungsorientiert. Ich konnte mich auf ihn verlassen und habe seine offene und freundliche Art geschätzt. Ich freue mich auf zukünftige Projekte mit ihm.'
+      name: 'M.Marrocu',
+      position: 'Frontend Developer',
+      zitat: 'Florian ist äußerst kreativ und findet immer Lösungsvorschläge, die das Team voranbringen. Seine Hilfsbereitschaft zeichnet ihn aus – er unterstützt andere jederzeit mit Rat und Tat. Durch sein starkes Engagement sorgt er dafür, dass Projekte erfolgreich umgesetzt werden.'
     },
   ]
 }
